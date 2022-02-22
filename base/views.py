cfe3d5c3a7d8a3dadf26b5347570e4a12046209c
stevescilar <stevemuambi@gmail.com>
2022-02-22 23:31:12 +0300

    studyBaze

--- base/views.py
+++ base/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 
+rooms = [
+    {'id':1,'name':'Lets learn Ps!'},
+    {'id':2,'name':'Make it Happen'},
+    {'id':3,'name':'Attitude'},
+]
+
+
 def home(request):
-    return render(request,'home.html')
+    return render(request,'home.html',{'rooms':rooms })
 
 def room(request):
     return render (request,'room.html')
