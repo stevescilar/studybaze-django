14c4c15b28b9769185ccdf66b3bbc92d19188138
stevescilar <stevemuambi@gmail.com>
2022-02-23 01:29:11 +0300

    solved

--- base/views.py
+++ base/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
+from .models import  Room
+ 
 
-
-rooms = [
-    {'id':1,'name':'Lets learn Ps!'},
-    {'id':2,'name':'Make it Happen'},
-    {'id':3,'name':'Attitude'},
-]
+# rooms = [
+#     {'id':1,'name':'Lets learn Ps!'},
+#     {'id':2,'name':'Make it Happen'},
+#     {'id':3,'name':'Attitude'},
+# ]
 
 
 def home(request):
-    # create a context dictationary
+    # querry set
+    rooms = Room.objects.all()
+    # create a context dictionary
     context = {'rooms':rooms}
     return render(request,'base/home.html',context) 
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id']== int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render (request,'base/room.html',context) 
