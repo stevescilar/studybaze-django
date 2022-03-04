f4e39e623d55852cfc4cfe09587512ade6b0d14a
stevescilar <stevemuambi@gmail.com>
2022-03-04 12:46:40 +0300

    divs

--- base/views.py
+++ base/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import  Room, Topic
+from .models import  Room, Topic ,Message
 from .forms import RoomForm
  
 
@@ -88,6 +88,16 @@ def home(request):
 def room(request,pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+
+
     context = {'room': room , 'room_messages':room_messages}
     return render (request,'base/room.html',context) 
 
