6a6fa8add3edf11417a9864fbca879cb23a061b9
stevescilar <stevemuambi@gmail.com>
2022-03-04 12:18:31 +0300

    messages

--- base/views.py
+++ base/views.py
@@ -87,8 +87,8 @@ def home(request):
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    messages = room.message_set.all()
-    context = {'room': room , 'messages':messages}
+    room_messages = room.message_set.all().order_by('-created')
+    context = {'room': room , 'room_messages':room_messages}
     return render (request,'base/room.html',context) 
 
 #login decorator
